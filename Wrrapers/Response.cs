66768137f4f784a63e8829bf5e738fc2c1d1bac0
Mateusz <mateusz.szczupak@outlook.com>
2022-11-08 12:00:27 +0100

    add response wraper

---
+++ Wrrapers/Response.cs
@@ -0,0 +1,26 @@
+﻿using SolarDynamicObservatoryWebService.Controllers;
+
+namespace SdoApi.Wrrapers
+{
+    public class Response<T>
+    {
+        public T Data { get; set; }
+        public bool Succeeded { get; set; }
+        public string Message { get; set; }
+        public string[] Errors { get; set; }
+
+        public Response()
+        {
+
+        }
+
+        public Response(T data)
+        {
+            Succeeded = true;
+            Message = string.Empty;
+            Errors = null;
+            Data = data;
+
+        }
+    }
+}
